2b048fea5f3c24f450af45e47fa5282a4155b5c1
tomkuku <61714983+tomkuku@users.noreply.github.com>
2022-10-30 16:51:51 +0100

    ✨ Make DatastoreObjectID public

---
+++ SwiftDatastore/SwiftDatastore/Core/DatastoreObjectID.swift
@@ -0,0 +1,27 @@
+//
+//  DatastoreObjectID.swift
+//  SwiftDatastore
+//
+//  Created by Kukułka Tomasz on 30/10/2022.
+//
+
+import Foundation
+import CoreData
+
+public final class DatastoreObjectID: CustomStringConvertible {
+    private let managedObjectID: NSManagedObjectID
+    
+    public var description: String {
+        managedObjectID.description
+    }
+    
+    init(managedObjectID: NSManagedObjectID) {
+        self.managedObjectID = managedObjectID
+    }
+}
+
+extension DatastoreObjectID: Equatable {
+    public static func == (lhs: DatastoreObjectID, rhs: DatastoreObjectID) -> Bool {
+        lhs.managedObjectID == rhs.managedObjectID
+    }
+}
