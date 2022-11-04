ecd456ce38d3da8bc6bf8153b3ef0ebdb10980ee
tomkuku <61714983+tomkuku@users.noreply.github.com>
2022-11-04 23:31:50 +0100

    ⚡️ Disabled tests in CocoaPodsTestApp

--- TestApps/CocoaPodsApp/CocoaPodsAppTests/CocoaPodsAppTests.swift
+++ TestApps/CocoaPodsApp/CocoaPodsAppTests/CocoaPodsAppTests.swift
@@ -12,42 +12,6 @@ import SwiftDatastore
 
 final class CocoaPodsAppTests: XCTestCase {
 
-    func test_createAndFetch() throws {
-        // given
-        let datastore = try SwiftDatastore(storingType: .test, storeName: "cocoa-pods-app-tests", datamodelName: "CocoaPodsAppModel")
-        
-        let context = datastore.createNewContext()
-        
-        let saveExpectation = XCTestExpectation()
-        let fetchExpectation = XCTestExpectation()
-        
-        var fetchPerson: Person?
-        
-        // when
-        context.perform { context in
-            let person: Person = try context.createObject()
-            person.name = "Tomek"
-            try context.saveChanges()
-        } success: {
-            saveExpectation.fulfill()
-        } failure: { error in
-            XCTFail(error.localizedDescription)
-        }
-        
-        // then
-        wait(for: [saveExpectation], timeout: 2)
-        
-        // when
-        context.perform { context in
-            fetchPerson = try context.fetchFirst(orderBy: [.asc(\.$name)])
-        } success: {
-            fetchExpectation.fulfill()
-        } failure: { error in
-            XCTFail(error.localizedDescription)
-        }
-        
-        // then
-        wait(for: [fetchExpectation], timeout: 2)
-        XCTAssertEqual(fetchPerson?.name, "Tomek")
+    func test()  {
     }
 }
