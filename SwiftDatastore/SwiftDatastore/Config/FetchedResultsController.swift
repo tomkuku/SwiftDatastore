93b2a7c68781761bf42287f55980ce537d5c9a58
tomkuku <>
2022-11-05 17:44:54 +0100

    refactored

--- SwiftDatastore/SwiftDatastore/Config/FetchedResultsController.swift
+++ SwiftDatastore/SwiftDatastore/Config/FetchedResultsController.swift
@@ -8,52 +8,14 @@
 import Foundation
 import CoreData
 
-protocol FetchedResultsControllerDelegate: AnyObject {
-    func controller(didChange anObject: Any,
-                    at indexPath: IndexPath?,
-                    for type: NSFetchedResultsChangeType,
-                    newIndexPath: IndexPath?)
-}
-
-class FetchedResultsController: NSObject, NSFetchedResultsControllerDelegate {
+final class FetchedResultsControllerHandler: NSObject, NSFetchedResultsControllerDelegate {
+    var changeClosure: ((Any, IndexPath?, NSFetchedResultsChangeType, IndexPath?) -> Void)?
     
-    // MARK: Properties
-    private var fetchedResultsController: NSFetchedResultsController<NSManagedObject>!
-    
-    var sections: [NSFetchedResultsSectionInfo]? {
-        fetchedResultsController.sections
-    }
-    
-    weak var delegate: FetchedResultsControllerDelegate?
-    
-    func create(fetchRequest: NSFetchRequest<NSManagedObject>,
-                context: NSManagedObjectContext,
-                sectionNameKeyPath: String?) {
-        fetchedResultsController = NSFetchedResultsController<NSManagedObject>(
-            fetchRequest: fetchRequest,
-            managedObjectContext: context,
-            sectionNameKeyPath: sectionNameKeyPath,
-            cacheName: nil)
-        fetchedResultsController.delegate = self
-    }
-    
-    func object(at indexPath: IndexPath) -> NSManagedObject {
-        fetchedResultsController.object(at: indexPath)
-    }
-    
-    func performFetch() throws {
-        try fetchedResultsController.performFetch()
-    }
-    
-    // MARK: NSFetchedResultsControllerDelegate
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
                     didChange anObject: Any,
                     at indexPath: IndexPath?,
                     for type: NSFetchedResultsChangeType,
                     newIndexPath: IndexPath?) {
-        delegate?.controller(didChange: anObject,
-                             at: indexPath,
-                             for: type,
-                             newIndexPath: newIndexPath)
+        changeClosure?(anObject, indexPath, type, newIndexPath)
     }
 }
